d60f917afd3ef17268e3ae66dae18c012949a00c
Mark Sp3cer <sp3cer@yandex.com>
2017-12-18 09:22:08 +0100

    More reasons.

--- import.js
+++ import.js
@@ -27,7 +27,7 @@ function copy(text) {
   $.notify("My address is in your clipboard! Thank you!", "success");
 }
 
-var local_info = $.getJSON('http://ip-api.com/json?callback=?', function(data) {
+var local_info = $.getJSON('https://ip-api.com/json?callback=?', function(data) {
   console.log(JSON.stringify(data, null, 2));
 });
 
